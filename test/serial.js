e8a8d8385a7fd77f4e9764d59c01b45943201039
Roger Schönbächler <schoero@gmx.ch>
2020-01-05 21:26:32 +0000

    added Serial RX

--- test/serial.js
+++ test/serial.js
@@ -5,9 +5,7 @@ const pigpio = require('pigpio');
 const Gpio = pigpio.Gpio;
 
 const outPin = 17;
-const output = new Gpio(outPin, {
-  mode: Gpio.OUTPUT
-});
+const output = new Gpio(outPin, {mode: Gpio.OUTPUT});
 
 let baud = 115200;
 let dataBits = 8;
@@ -19,6 +17,17 @@ output.waveAddSerial(baud, dataBits, stopBits, offset, message);
 
 let waveId = output.waveCreate();
 
+// output.glitchFilter(Math.ceil(0.001 / baud * 2));
+
+output.serialReadOpen(baud, dataBits);
+output.enableAlert();
+
+output.on('alert', (level, tick) => {
+  console.log('alert');
+  let data = output.serialRead();
+  console.log(data);
+});
+
 if(waveId >= 0) {
   output.waveTxSend(waveId, pigpio.WAVE_MODE_ONE_SHOT);
 }
@@ -26,3 +35,4 @@ if(waveId >= 0) {
 while (output.waveTxBusy()) { }
 
 output.waveDelete(waveId);
+output.disableAlert();
